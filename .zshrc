82113e180ad18fe30039473b65a42472739a5cb1
Muhammad Atif Ali <me@matifali.dev>
2023-08-20 02:43:54 +0300

    update

--- .zshrc
+++ .zshrc
@@ -1,7 +1,8 @@
 export ZSH="$HOME/.oh-my-zsh"
-export LANG=en_US.UTF-8
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
 export EDITOR='vim'
-ZSH_THEME="agnoster"
-plugins=(git zsh-autosuggestions zsh-completions)
 source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+ZSH_THEME="agnoster"
+plugins=(git zsh-autosuggestions zsh-completions zsh-syntax-highlighting)
 source $ZSH/oh-my-zsh.sh
--- install.sh
+++ install.sh
@@ -1,60 +1,53 @@
 #!/bin/bash
 
-# Check if Zsh is installed, if not, install it
+# Check if Zsh is installed and install it if not
 if ! command -v zsh &> /dev/null; then
-    echo "Zsh not found. Installing..."
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        sudo apt update && sudo apt install zsh -y
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        # Check if Homebrew is installed, if not, install it
-        if ! command -v brew &> /dev/null; then
-            echo "Homebrew not found. Installing..."
-            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        fi
-        brew install zsh
-    else
-        echo "Unsupported OS. Please install Zsh manually."
-        exit 1
-    fi
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt update
+    sudo apt install zsh
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install zsh
+  fi
 fi
 
-# Make Zsh the default shell
-chsh -s $(which zsh)
+# Set Zsh as default shell
+if [ "$SHELL" != "$(which zsh)" ]; then
+  chsh -s $(which zsh)
+fi
 
-# Install Oh My Zsh
+# Install Oh My Zsh if not already installed
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
-# Install zsh-autosuggestions
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Define the installation directory for the plugins
+PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
+
+# Install Zsh plugins if not already installed
+if [ ! -d "$PLUGINS_DIR/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions $PLUGINS_DIR/zsh-autosuggestions
 fi
 
-# Install zsh-completions
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions" ]; then
-    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+if [ ! -d "$PLUGINS_DIR/zsh-completions" ]; then
+  git clone https://github.com/zsh-users/zsh-completions $PLUGINS_DIR/zsh-completions
 fi
 
-# Install zsh-syntax-highlighting
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ ! -d "$PLUGINS_DIR/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $PLUGINS_DIR/zsh-syntax-highlighting
 fi
 
 # Link the .zshrc file
 DOTFILES_DIR=$(pwd)
+# backup existing .zshrc file
+if [ -f ~/.zshrc ]; then
+  mv ~/.zshrc ~/.zshrc.bak
+fi
 ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
 
-# Update the .zshrc file based on the operating system
+# Add brew and macos plugins for macOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "Adding brew and macos plugins for macOS"
-    sed -i '' 's/plugins=(git zsh-autosuggestions zsh-completions)/plugins=(git zsh-autosuggestions zsh-completions brew macos)/' .zshrc
+  echo "Adding brew and macos plugins for macOS"
+  perl -i -pe 's/plugins=\(/plugins=(brew macos /' ~/.zshrc
 fi
 
-# Reload Zsh configuration
-source ~/.zshrc
-
-# Reload Zsh configuration
-source ~/.zshrc
-
-echo "Dotfiles
+echo "Dotfiles installation complete!"
