0fc26cba040ac538046060b5cc86d41e4289771e
Muhammad Atif Ali <me@matifali.dev>
2023-12-04 12:53:18 +0300

    Update editor and add new configurations

--- .zshrc
+++ .zshrc
@@ -6,3 +6,15 @@ source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-sy
 ZSH_THEME="agnoster"
 plugins=(git zsh-autosuggestions zsh-completions zsh-syntax-highlighting)
 source $ZSH/oh-my-zsh.sh
+
+# bun completions
+[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# flyctl
+export FLYCTL_INSTALL="$HOME/.fly"
+export PATH="$FLYCTL_INSTALL/bin:$PATH"
+compdef _flyctl fly
--- install.sh
+++ install.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
+DOTFILES_DIR="$(realpath "$(dirname "$0")")"
+echo "DOTFILES_DIR: $DOTFILES_DIR"
+
 # Check if Zsh is installed and install it if not
 if ! command -v zsh &>/dev/null; then
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Installing Zsh"
     sudo apt-get update
     sudo apt-get install zsh -y
   elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install zsh
   fi
+else
+  echo "Zsh is already installed"
 fi
 
 # Set Zsh as default shell
 if [ "$SHELL" != "$(which zsh)" ]; then
+  echo "Setting Zsh as default shell for $USER"
   sudo chsh -s $(which zsh) $USER
+else
+  echo "Zsh is already the default shell"
 fi
 
 # Install Oh My Zsh if not already installed
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "Installing Oh My Zsh"
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+  echo "Oh My Zsh is already installed"
 fi
 
 # Define the installation directory for the plugins
@@ -25,30 +37,41 @@ PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
 
 # Install Zsh plugins if not already installed otherwise update them
 if [ ! -d "$PLUGINS_DIR/zsh-autosuggestions" ]; then
+  echo "Installing zsh-autosuggestions"
   git clone https://github.com/zsh-users/zsh-autosuggestions $PLUGINS_DIR/zsh-autosuggestions
 else
+  echo "Updating zsh-autosuggestions"
   cd $PLUGINS_DIR/zsh-autosuggestions && git pull
 fi
 
 if [ ! -d "$PLUGINS_DIR/zsh-completions" ]; then
+  echo "Installing zsh-completions"
   git clone https://github.com/zsh-users/zsh-completions $PLUGINS_DIR/zsh-completions
 else
+  echo "Updating zsh-completions"
   cd $PLUGINS_DIR/zsh-completions && git pull
 fi
 
 if [ ! -d "$PLUGINS_DIR/zsh-syntax-highlighting" ]; then
+  echo "Installing zsh-syntax-highlighting"
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $PLUGINS_DIR/zsh-syntax-highlighting
 else
+  echo "Updating zsh-syntax-highlighting"
   cd $PLUGINS_DIR/zsh-syntax-highlighting && git pull
 fi
 
 # Link the .zshrc file
-DOTFILES_DIR="$(dirname "$0")" # Set DOTFILES_DIR to the directory where the script is located
 # backup existing .zshrc file
 if [ -f ~/.zshrc ]; then
   mv ~/.zshrc ~/.zshrc.bak
 fi
 ln -sf "$DOTFILES_DIR/.zshrc" ~/.zshrc
+
+# Link the .config/nix/nix.conf file
+ln -sf "$DOTFILES_DIR/.config/nix/nix.conf" ~/.config/nix/nix.conf
+
+# Link the .config/ghostty/ghostty.conf file
+ln -sf "$DOTFILES_DIR/.config/ghostty/ghostty.conf" ~/.config/ghostty/ghostty.conf
 
 # Add brew and macos plugins for macOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
